c15ab7acc8ceffceb42abc5c20743cb0c1afd7eb
0xcacti <0xcacti@gmail.com>
2023-05-18 17:55:43 -0400

    add first iteration start value

--- src/NightHam.sol
+++ src/NightHam.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.19;
 
+import "forge-std/Test.sol";
+
 import { IPuzzle } from "./interfaces/IPuzzle.sol";
 
 contract NightHam is IPuzzle {
@@ -11,10 +13,8 @@ contract NightHam is IPuzzle {
 
     /// @inheritdoc IPuzzle
     function generate(address _seed) external pure returns (uint256) {
-        // you need to figure out a way to encode a knight's movement
-        // I assume the board looks like a bytes 32 with one spot fulfilled
-        //
         uint256 seed = uint256(keccak256(abi.encodePacked(_seed)));
+        return 7 << (seed % 64) * 4;
     }
 
     /// @inheritdoc IPuzzle
