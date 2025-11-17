d411e382f47a7dee5254a9485272a4d58f4046e4
0xcacti <0xcacti@gmail.com>
2025-11-17 03:37:59 -0500

    I've confirmed it's probably not possible, maaaaybe there is some distant possibilyt

--- lua/grid/init.lua
+++ lua/grid/init.lua
@@ -3,7 +3,8 @@ local M = {}
 function M.grid()
     local buf = vim.api.nvim_create_buf(false, true)
     local ui = vim.api.nvim_list_uis()[1] or { width = 80, height = 24 }
-    local w, h = 10, 1
+    local w, h = 2, 1
+
     local opts = {
         relative = "editor",
         width = w,
@@ -11,15 +12,28 @@ function M.grid()
         row = math.floor((ui.height - h) / 2),
         col = math.floor((ui.width - w) / 2),
         style = "minimal",
-        border = "single",
+        border = "none",
         focusable = false,
         zindex = 200,
     }
-    vim.api.nvim_buf_set_lines(buf, 0, -1, false, { " " })
+    local opts2 = {
+        relative = "editor",
+        width = w,
+        height = h,
+        row = math.floor((ui.height - h) / 2) + 1,
+        col = math.floor((ui.width - w) / 2),
+        style = "minimal",
+        border = "none",
+        focusable = false,
+        zindex = 200,
+    }
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "■■" })
     local win = vim.api.nvim_open_win(buf, false, opts)
+    local win2 = vim.api.nvim_open_win(buf, false, opts2)
     vim.api.nvim_set_hl(0, "GridNormal", { bg = "#1f2335" })
-    vim.api.nvim_set_hl(0, "GridBorder", { fg = "#7aa2f7" })
-    vim.api.nvim_win_set_option(win, "winhl", "NormalFloat:GridNormal,FloatBorder:GridBorder")
+    -- vim.api.nvim_set_hl(0, "GridBorder", { bg = "#7aa2f7" })
+    vim.api.nvim_win_set_option(win, "winhl", "NormalFloat:GridNormal")
+    vim.api.nvim_win_set_option(win2, "winhl", "NormalFloat:GridNormal")
 end
 
 function M.setup()
