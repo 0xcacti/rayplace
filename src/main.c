b9ab8280c7eb7988892d60d1709075cc69dc45bd
0xcacti <0xcacti@gmail.com>
2025-11-26 18:16:02 -0700

    draw window in black

--- src/main.c
+++ src/main.c
@@ -1,14 +1,21 @@
 #include "raylib.h"
 
 int main(void) {
+  int windowWidth = 400;
+  int windowHeight = 150;
+
   SetConfigFlags(FLAG_WINDOW_UNDECORATED);
-  InitWindow(600, 600, "rayplace");
+  InitWindow(windowWidth, windowHeight, "rayplace");
+
+  int screenWidth = GetMonitorWidth(0);
+  int screenHeight = GetMonitorHeight(0);
+
+  SetWindowPosition(screenWidth / 2 - windowWidth / 2, screenHeight / 2 - windowHeight / 2);
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BLACK);
-    DrawText("Hello, raylib!", 300, 280, 20, DARKGRAY);
     EndDrawing();
   }
 
